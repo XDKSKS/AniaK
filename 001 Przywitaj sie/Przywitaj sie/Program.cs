c3b351f432f7422afc23a70c15d36a1b6790690f
krolanna <krolanna@105-12>
2020-02-19 11:27:36 +0100

    001

---
+++ 001 Przywitaj sie/Przywitaj sie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przywitaj_sie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programuję więc jestem!");
+            Console.ReadKey();
+
+        }
+    }
+}
