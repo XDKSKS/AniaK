39a09fc978254852a9388ca3ca0b4ba88db7c1bc
krolanna <krolanna@105-12>
2020-02-21 13:01:42 +0100

    004

---
+++ 004 Argumenty/Argumenty/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Argumenty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witam");
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
